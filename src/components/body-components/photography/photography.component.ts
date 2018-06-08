02a42e5703ddb9b6455f9f75e217c725d8fac5bd
Alex Keyport <Alex Keyport>
2018-06-08 18:47:47 -0500

    test

--- src/components/body-components/photography/photography.component.ts
+++ src/components/body-components/photography/photography.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
 
 @Component({
   selector: 'app-photography-component',
   templateUrl: './photography.component.html',
-  styleUrls: ['./photography.component.scss'],
+  styleUrls: ['./photography.component.scss']
 })
 export class PhotographyComponent {
 }
